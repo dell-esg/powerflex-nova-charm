d4093aaee10367950ddcda11ed89c3153125f065
jproque-dell <jeanpierre.roquesalane@dell.com>
2024-12-02 13:45:14 +0100

    Update charm.py

--- src/charm.py
+++ src/charm.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Charmed operator for Dell PowerFlex Nova access."""
+
 import os
 import subprocess
 
